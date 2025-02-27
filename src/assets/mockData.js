b619bbcbc21419ffc09deb7e7ec44a03bb0271e1
Eira Birkhammar <eira.birkhammar@quale.se>
2025-02-27 19:44:49 +0100

    Added mock data

---
+++ src/assets/mockData.js
@@ -0,0 +1,86 @@
+[
+    {
+      "id": "H1",
+      "reference_designation": "=H-H1+L1",
+      "name": "Furnace 1",
+      "type": "Heating",
+      "location": "Line 1",
+      "status": "Active",
+      "sub_components": [
+        {
+          "id": "H1-B1",
+          "reference_designation": "=H-H1-B1+L1",
+          "name": "Burner Unit",
+          "type": "Burner",
+          "status": "Active"
+        },
+        {
+          "id": "H1-F1",
+          "reference_designation": "=H-H1-F1+L1",
+          "name": "Furnace Blower",
+          "type": "Blower",
+          "status": "Operational"
+        },
+        {
+          "id": "H1-S1",
+          "reference_designation": "=H-H1-S1+L1",
+          "name": "Temperature Sensor",
+          "type": "Sensor",
+          "status": "Functional"
+        },
+        {
+          "id": "H1-C1",
+          "reference_designation": "=H-H1-C1+L1",
+          "name": "Control Panel",
+          "type": "Control System",
+          "status": "Online"
+        }
+      ]
+    },
+    {
+      "id": "H2",
+      "reference_designation": "=H-H2+L2",
+      "name": "Furnace 2",
+      "type": "Heating",
+      "location": "Line 2",
+      "status": "Maintenance",
+      "sub_components": []
+    },
+    {
+      "id": "R1",
+      "reference_designation": "=R-R1+L1",
+      "name": "Roughing Mill Stand 1",
+      "type": "Rolling",
+      "location": "Line 1",
+      "status": "Active",
+      "sub_components": []
+    },
+    {
+      "id": "R2",
+      "reference_designation": "=R-R2+L2",
+      "name": "Finishing Mill Stand 2",
+      "type": "Rolling",
+      "location": "Line 2",
+      "status": "Idle",
+      "sub_components": []
+    },
+    {
+      "id": "C1",
+      "reference_designation": "=C-C1+L1",
+      "name": "Cooling Bed 1",
+      "type": "Cooling",
+      "location": "Line 1",
+      "status": "Active",
+      "sub_components": []
+    },
+    {
+      "id": "S1",
+      "reference_designation": "=S-S1+M1",
+      "name": "Shear Cutter 1",
+      "type": "Shearing",
+      "location": "Maintenance Area",
+      "status": "Inactive",
+      "sub_components": []
+    }
+  ]
+  
